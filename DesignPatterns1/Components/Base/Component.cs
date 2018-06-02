ae3a34e83bd221bf3d9843ce57fb68cbcc36eff8
BenSchoenmakers94 <bp.schoenmakers@student.avans.nl>
2018-06-02 18:51:36 +0200

    styling

--- DesignPatterns1/Components/Base/Component.cs
+++ DesignPatterns1/Components/Base/Component.cs
@@ -10,41 +10,48 @@ namespace DesignPatterns1.Nodes.Base
 {
     public abstract class Component
     {
-
         private string _name;
         private string _typeName;
         private List<Component> _outputComponents;
+        private List<bool> _inputs;
         private bool _result;
         private long _startTime;
         private long _endTime;
 
-
-
-
         public Component(string name)
         {
-
+            _name = name;
         }
 
         public void AddOutputComponent(Component outputComponent)
         {
-
+            _outputComponents.Add(outputComponent);
         }
 
         public void ReceiveInput(bool input)
         {
-
+            _inputs.Add(input);
         }
 
         public void Reset()
         {
-
+            _name = "";
+            _outputComponents.Clear();
+            _inputs.Clear();
+            _result = false;
+            _startTime = 0;
+            _endTime = 0;
         }
 
-        override
-        public string ToString()
+        public void Run()
         {
-            return "";
+            StartTime();
+
+            CalculateResult();
+
+            FeedResultToNext();
+
+            EndTime();
         }
 
         public bool CalculateResult()
@@ -54,17 +61,19 @@ namespace DesignPatterns1.Nodes.Base
 
         public void FeedResultToNext()
         {
-
+            _outputComponents.ForEach((Component component) => {
+                component.ReceiveInput(_result);
+            });
         }
 
         public void StartTime()
         {
-
+            _startTime = NanoTime();
         }
 
         public void EndTime()
         {
-
+            _endTime = NanoTime();
         }
 
         public void Accept(IVisitor visitor)
